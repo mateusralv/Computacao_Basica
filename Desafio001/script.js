379de155dbfa916028465f40b98ffbeaca8a4b7e
Mateus Alves <alv.mateus@outlook.com>
2020-05-30 14:25:17 -0300

    Desafio 001
    Resolução do desafio 001 proposto pelo Professor Guanabara

---
+++ Desafio001/script.js
@@ -0,0 +1,11 @@
+function botao1() {
+    window.alert("Você apertou o botão 1")
+}
+
+function botao2() {
+    window.alert("Você apertou o botão 2!")
+}
+
+function botao3() {
+    window.alert("Você apertou o botão 3!")
+}
