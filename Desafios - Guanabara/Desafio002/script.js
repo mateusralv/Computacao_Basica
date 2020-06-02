e86979625ff02570059aed77f0fd6a4fb040dab0
Mateus Alves <alv.mateus@outlook.com>
2020-06-01 23:10:29 -0300

    Desafio002
    Resolução para o desafio 002 em JS

---
+++ Desafios - Guanabara/Desafio002/script.js
@@ -0,0 +1,5 @@
+function comecar(){
+    var nome = prompt("Olá qual seu nome?")
+    var idade = prompt(`Olá ${nome}, tudo bem? Qual tua idade?`)
+    alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
