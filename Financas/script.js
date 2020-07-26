4a132651189633a5fca36fed89fc8bd24ac192d2
Mateus Alves <alv.mateus@outlook.com>
2020-07-26 20:32:14 -0300

    update

---
+++ Financas/script.js
@@ -0,0 +1,11 @@
+const data = "29 JUN-Mateus-Recargacel-	25,00 29 JUN Uberbr Uber Trip Help.	3,82"
+
+let teste = {
+    data: ['29 JUN','27 JUN'],
+    responsavel: ['Mateus','Alan'],
+    descricao: ['recargaCel','Uber'],
+    valor: [25.00,3.82]
+}
+
+console.log(teste)
+
